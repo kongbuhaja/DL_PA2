ba4fe98f24d3419cfd3a57f17e79c998eef025c1
kongbuhaja <pcar530@gmail.com>
2024-06-27 13:13:08 +0000

    update etc

--- shell.sh
+++ shell.sh
@@ -7,7 +7,7 @@ start_background_task() {
             --batch-size 64 \
             --epochs 300 \
             --cfg "${cfg}" \
-            --weights yolov5n.pt \
+            --weights runs/train/yolov5n2/weights/ap_best.pt \
             --rgbt \
             --metric "${2}" \
             --name "${name}" > "${name}.log" 2>&1 &
@@ -17,7 +17,7 @@ wait_for_completion() {
     local path="models/yolov5n_kaist-rgbt"
     local cfg="${path}${1}.yaml"
     local name="yolov5n${1}"
-    while pgrep -f "python3 train_simple.py --img 640 --batch-size 64 --epochs 300 --cfg ${cfg} --weights yolov5n.pt --rgbt --metric ${2} --name ${name}" > /dev/null; do
+    while pgrep -f "python3 train_simple.py --img 640 --batch-size 64 --epochs 600 --cfg ${cfg} --weights runs/train/yolov5n2/weights/ap_best.pt --rgbt --metric ${2} --name ${name}" > /dev/null; do
         sleep 30
     done
 }
@@ -28,11 +28,11 @@ task() {
 }
 
 # task "" "map"
-task "_anchors" "map"
-task "_sq_anchors" "map"
+# task "_anchors" "map"
+# task "_sq_anchors" "map"
 # task "" "mr"
 # task "_anchors" "mr"
 # task "" "hybrid"
 # task "_anchors" "hybrid"
 
-# sh utils/eval/shell.sh
+sh utils/eval/shell.sh
--- utils/eval/shell.sh
+++ utils/eval/shell.sh
@@ -1,9 +1,9 @@
-run=/home/DL_PA2/runs/val/
-save=/home/DL_PA2/runs/train/
 util=/home/DL_PA2/utils/eval/
+save=${util}comparison/
 
 python3 ${util}kaisteval.py --annFile ${util}KAIST_annotation.json \
-                            --rstFiles ${save}/yolov5n2/epoch296_predictions.json \
-                                       ${save}/yolov5n2/epoch28_predictions.json \
-                                       ${save}/yolov5n2/epoch100_predictions.json \
-                            --evalFig "${run}ap_benchmark.jpg"
+                            --rstFiles ${save}yolov5n_base.json \
+                                       ${save}yolov5n_new_anchor.json \
+                                       ${save}yolov5n_samp_anchor.json \
+                                       ${save}yolov5n_base_more.json \
+                            --evalFig ${save}mr_benchmark.jpg
